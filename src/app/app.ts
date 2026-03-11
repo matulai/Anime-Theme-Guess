5a216baa4adf24a982ca8b3696e3745d7410f37a
Matias Laime <matulai1221@gmail.com>
2026-03-10 22:20:54 -0300

    fix: wrong colors, pages without router and routes alias on ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  imports: [RouterOutlet],
 })
 export class App {
   protected readonly title = signal('Theme-Guess');
