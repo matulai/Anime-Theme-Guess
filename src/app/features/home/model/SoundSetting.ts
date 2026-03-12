e1c8ef6a4dccf3713a6185ebc305ac07c6a760e7
Matias Laime <matulai1221@gmail.com>
2026-03-12 16:26:00 -0300

    Refactor: types on home

--- src/app/features/home/model/SoundSetting.ts
+++ src/app/features/home/model/SoundSetting.ts
@@ -1,10 +1,7 @@
+import { Description } from './Description';
+
 export interface SoundSetting {
   settingTitle: string;
   volume: number;
   description: Description[];
-}
-
-interface Description {
-  type: string;
-  content: { text: string; bold?: boolean }[];
 }
