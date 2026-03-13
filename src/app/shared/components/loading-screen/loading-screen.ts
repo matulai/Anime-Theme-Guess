aa95ee3f4874a237cab32cca2fae1edef5b9ac6e
Matias Laime <matulai1221@gmail.com>
2026-03-12 22:19:11 -0300

    Add: loading screen component

---
+++ src/app/shared/components/loading-screen/loading-screen.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { LoadingService } from '@core/services/loading-service';
+
+@Component({
+  selector: 'app-loading-screen',
+  imports: [],
+  templateUrl: './loading-screen.html',
+  styleUrl: './loading-screen.scss',
+  host: {
+    '(window:keydown)': 'handlePressButton()',
+  },
+})
+export class LoadingScreen {
+  loadingService = inject(LoadingService);
+
+  state = this.loadingService.state;
+
+  start() {
+    if (this.state() === 'ready') {
+      this.loadingService.hide();
+    }
+  }
+
+  handlePressButton() {
+    this.start();
+  }
+}
