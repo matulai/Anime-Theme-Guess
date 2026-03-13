6616abf10e4a4c524b2930f93dba83dc400d1663
Matias Laime <matulai1221@gmail.com>
2026-03-12 22:20:11 -0300

    Feat: loading screen on all app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingScreen } from '@shared/components/loading-screen/loading-screen';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingScreen],
 })
 export class App {
   protected readonly title = signal('Theme-Guess');
