fcc3122b894987c9f44604224c6c28439e4a40dc
Matias Laime <matulai1221@gmail.com>
2026-03-23 18:44:47 -0300

    Add: working full videos and audios

--- src/app/features/game/pages/game/game.ts
+++ src/app/features/game/pages/game/game.ts
@@ -29,15 +29,18 @@ export class Game implements OnDestroy, AfterViewInit {
   gameService = inject(GameService);
   private router = inject(Router);
 
-  audioPlayer = viewChild.required<ElementRef<HTMLAudioElement>>('audioPlayer');
-  videoPlayer = viewChild.required<ElementRef<HTMLVideoElement>>('videoPlayer');
+  audioPlayer1 = viewChild.required<ElementRef<HTMLAudioElement>>('audioPlayer1');
+  audioPlayer2 = viewChild.required<ElementRef<HTMLAudioElement>>('audioPlayer2');
+
+  videoPlayer1 = viewChild.required<ElementRef<HTMLVideoElement>>('videoPlayer1');
+  videoPlayer2 = viewChild.required<ElementRef<HTMLVideoElement>>('videoPlayer2');
+
   settings: {
     gameSettings: GameSettingOptions;
     soundSettings: SoundSettingOptions;
   };
 
   isLoadingNextTrack = signal(true);
-  isAudioPlaying = signal(false);
   isVideoVisible = signal(false);
 
   timerSeconds!: Signal<number>;
@@ -51,92 +54,77 @@ export class Game implements OnDestroy, AfterViewInit {
     this.timerSeconds = signal(10);
   }
 
-  ngOnDestroy() {
-    const audio = this.audioPlayer()?.nativeElement;
-    const video = this.videoPlayer()?.nativeElement;
+  async ngAfterViewInit() {
+    this.loadingService.showLoading();
 
-    audio?.removeEventListener('ended', this.onAudioEnded);
-    video?.removeEventListener('ended', this.onVideoEnded);
+    await this.gameService.setupGame();
 
-    this.loadingService.hide();
+    const audioPlayer1 = this.audioPlayer1().nativeElement;
+    const audioPlayer2 = this.audioPlayer2().nativeElement;
+
+    const videoPlayer1 = this.videoPlayer1().nativeElement;
+    const videoPlayer2 = this.videoPlayer2().nativeElement;
+
+    await this.gameService.preloadNextAudioTrack(audioPlayer1);
+    await this.gameService.preloadNextVideoTrack(videoPlayer1);
+
+    this.gameService.nextTrack();
+
+    audioPlayer1.onended = () => this.onAudioEnded(videoPlayer1, audioPlayer1, audioPlayer2);
+    videoPlayer1.onended = () => this.onVideoEnded(audioPlayer2, videoPlayer1, videoPlayer2);
+    audioPlayer2.onended = () => this.onAudioEnded(videoPlayer2, audioPlayer2, audioPlayer1);
+    videoPlayer2.onended = () => this.onVideoEnded(audioPlayer1, videoPlayer2, videoPlayer1);
+
+    this.loadingService.showPressStart(() => this.startGame());
   }
 
   navigateTo(route: string) {
     this.router.navigate([route]);
   }
 
-  startGame(): void {
-    this.audioPlayer().nativeElement.play();
+  async startGame(): Promise<void> {
+    await this.audioPlayer1().nativeElement.play();
     this.timerRunning.set(true);
   }
 
-  async ngAfterViewInit() {
-    this.loadingService.showLoading();
-
-    await this.gameService.setupGame();
-
-    await this.gameService.preloadNextAudioTrack();
-    await this.gameService.preloadNextVideoTrack();
-
-    this.gameService.nextTrack();
-
-    const audio = this.audioPlayer().nativeElement;
-    const video = this.videoPlayer().nativeElement;
-
-    audio.addEventListener('ended', this.onAudioEnded);
-    video.addEventListener('ended', this.onVideoEnded);
-
-    const track = this.gameService.currentTrack();
-    if (!track) return;
-
-    audio.src = track.audioUrl;
-    video.src = track.videoUrl;
-
-    audio.load();
-    video.load();
-
-    this.loadingService.showPressStart(() => this.startGame());
-  }
-
-  onAudioEnded = () => {
+  async onAudioEnded(
+    videoPlayer: HTMLVideoElement,
+    actualAudioPlayer: HTMLAudioElement,
+    nextAudioPlayer: HTMLAudioElement,
+  ): Promise<void> {
+    await videoPlayer.play();
     this.isVideoVisible.set(true);
-    this.videoPlayer().nativeElement.play();
 
-    const audio = this.audioPlayer().nativeElement;
+    actualAudioPlayer.pause();
+    actualAudioPlayer.removeAttribute('src');
+    actualAudioPlayer.load();
 
-    this.gameService.preloadNextAudioTrack();
+    await this.gameService.preloadNextAudioTrack(nextAudioPlayer);
+  }
 
-    const nextTrack = this.gameService.getNextTrack();
-
-    if (!nextTrack) return;
-
-    audio.src = nextTrack.audioUrl;
-
-    audio.load();
-  };
-
-  onVideoEnded = () => {
+  async onVideoEnded(
+    audioPlayer: HTMLAudioElement,
+    actualVideoPlayer: HTMLVideoElement,
+    nextVideoPlayer: HTMLVideoElement,
+  ): Promise<void> {
     this.isVideoVisible.set(false);
-    this.audioPlayer().nativeElement.play();
+    await audioPlayer.play();
     this.timerRunning.set(true);
-
-    const video = this.videoPlayer().nativeElement;
-
-    this.gameService.preloadNextVideoTrack();
-
-    this.gameService.nextTrack();
-
-    const nextTrack = this.gameService.currentTrack();
-    if (!nextTrack) return;
-
-    video.src = nextTrack.videoUrl;
-
-    video.load();
-
     this.currentTracks.update((v) => ++v);
-  };
+
+    actualVideoPlayer.pause();
+    actualVideoPlayer.removeAttribute('src');
+    actualVideoPlayer.load();
+
+    await this.gameService.preloadNextVideoTrack(nextVideoPlayer);
+    this.gameService.nextTrack();
+  }
 
   onTimerEnd() {
     this.timerRunning.set(false);
+  }
+
+  ngOnDestroy() {
+    this.loadingService.hide();
   }
 }
